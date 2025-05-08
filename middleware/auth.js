dd3de76e24657f840cf6ecabb74b9c3eaa5bfd0d
FelipeFontanaSeganfredo <felipefseganfredo@gmail.com>
2025-05-07 23:28:42 -0300

    Pequena correção

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const verificarToken = (req, res, next) => {
+
     const token = req.headers['authorization'];
 
     if (!token) return res.status(403).json({ message: 'Token não fornecido' });
